7edef8532cd74da2a20c30bb136b9b85ae0cfc7b
Comrad Alex <s.aleksandras@gmail.com>
2021-12-14 05:05:17 +0200

    ...

--- UqWordsOptimized0/sources/main.cpp
+++ UqWordsOptimized0/sources/main.cpp
@@ -7,8 +7,9 @@
 #include <algorithm>
 #include <semaphore>
 #include <ranges>
-
-#include <fmt/core.h>
+#include <list>
+#include <cassert>
+#include <fmt/format.h>
 
 #include "concurrent_queue.hpp"
 #include "thread_pool.hpp"
@@ -18,20 +19,30 @@ struct word_set_scan
 {
   using word_set_container = std::unordered_set<std::string>;
 
-  struct task_context
+  struct scan_task_context
   {
     mmap_wrapper m_mmap;
     std::string_view m_view;
 
-    task_context(mmap_wrapper mmap, std::string_view view)
+    scan_task_context(mmap_wrapper mmap, std::string_view view)
     : m_mmap { std::move(mmap) }
     , m_view { std::move(view) }
     {}    
   };
 
+  struct merge_task_context
+  {
+    word_set_container m_lhs;
+    word_set_container m_rhs;
+
+    merge_task_context(word_set_container lhs, word_set_container rhs)
+    : m_lhs { std::move(lhs) }
+    , m_rhs { std::move(rhs) }
+    {}    
+  };
+
   word_set_scan (std::size_t block_size = 1024*1024, std::uint32_t num_threads = std::thread::hardware_concurrency())
   : m_num_threads       { num_threads },
-    m_intermediate_sets { std::make_unique<word_set_container[]>(num_threads) },
     m_tasks_waiting     { std::uint32_t(num_threads * 16u) },
     m_thread_pool       { num_threads },
     m_block_size        { block_size & mmap_wrapper::alignment_mask() }
@@ -62,45 +73,67 @@ struct word_set_scan
       bytes_remaining -= last_space_off;
       
       // Using shared ptr to work around limitations of std::function later on :/
-      auto context_ptr = std::make_shared<task_context>(std::move (handle), std::move (block_view));
-      m_thread_pool.enqueue ([this, context_ptr { std::move (context_ptr) }] (std::size_t index) {
-        perform_set_collection_task(*context_ptr, index);        
-        m_tasks_waiting.release();
-      });
+      auto context_ptr = std::make_shared<scan_task_context>(std::move (handle), std::move (block_view));
+      m_intermediate.emplace_back(m_thread_pool.async ([this, context_ptr { std::move (context_ptr) }] () {
+        return perform_set_collection_task(*context_ptr);        
+      }));
     }
-    m_thread_pool.wait_for_all();
-    word_set_container master_set;
-    for(auto i = 0; i < m_num_threads; ++i)
-      master_set.merge(m_intermediate_sets[i]);
+    
+    while(m_intermediate.size() > 1)
+    {
+      word_set_container lhs = std::move (m_intermediate.front().get());
+      m_intermediate.pop_front();
+      word_set_container rhs = std::move (m_intermediate.front().get());
+      m_intermediate.pop_front();
+
+      auto context_ptr = std::make_shared<merge_task_context>(std::move (lhs), std::move (rhs));
+
+      m_intermediate.emplace_back(m_thread_pool.async (
+        [this, context_ptr { std::move (context_ptr) }] () 
+          -> word_set_container 
+        {
+          auto result_set = std::move (context_ptr->m_lhs);
+          result_set.merge(std::move (context_ptr->m_rhs));
+          return result_set;
+        }));
+    }
+    
+    assert(m_intermediate.size() == 1);
+    auto master_set = std::move (m_intermediate.front().get());
+
     std::cout << master_set.size() << "\n";
   }
 
-  void perform_set_collection_task(task_context& context, std::size_t index)
+  auto perform_set_collection_task(scan_task_context& context)
+    -> word_set_container
   {
     using namespace std;
     using namespace string_view_literals;
     using namespace string_literals;
 
+    unordered_set<string> local_word_set;
+
     auto& view  = context.m_view;
     auto& mmap  = context.m_mmap;
 
-    std::unordered_set<std::string>& local_word_set = m_intermediate_sets[index];
     auto start_here = view.find_first_not_of(' ');    
-    while (start_here != std::string_view::npos)
+    while (start_here != string_view::npos)
     {
       auto end_here = view.find_first_of(' ', start_here);
       auto word = view.substr(start_here, end_here - start_here);
       start_here = view.find_first_not_of(' ', end_here);
-      local_word_set.emplace(word);      
+      local_word_set.emplace(word.begin(), word.end());      
     }
+    m_tasks_waiting.release();
+    return local_word_set;
   }
 
 private:
   const std::size_t m_num_threads;
-  std::unique_ptr<word_set_container []> m_intermediate_sets;
   std::counting_semaphore<> m_tasks_waiting;
   thread_pool m_thread_pool;
   const std::size_t m_block_size;
+  std::list<std::future<word_set_container>> m_intermediate;
 };
 
 int main(int argc, char** argv)
