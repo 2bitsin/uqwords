c3d8e9f8f793251a12129b43c868247fa9e2d164
Comrad Alex <s.aleksandras@gmail.com>
2021-12-15 14:50:58 +0200

    Cleaning up code

--- UqWordsOptimized/sources/parallel_split_and_reduce.hpp
+++ UqWordsOptimized/sources/parallel_split_and_reduce.hpp
@@ -3,11 +3,11 @@
 #include <unordered_set>
 #include <algorithm>
 #include <ranges>
+#include <vector>
 
 #include "concurrent_queue.hpp"
 #include "parallel_task_dispatch.hpp"
 #include "chunk_loader.hpp"
-#include "parallel_concurrent_queue.hpp"
 #include "pinned_object.hpp"
 
 template <typename _Reduce_target>
@@ -16,14 +16,13 @@ struct parallel_split_and_reduce: pinned_object
   using reduce_target_type = _Reduce_target;
   using reduce_merge_type = std::tuple<reduce_target_type, reduce_target_type>;
 
-  parallel_split_and_reduce (std::uint32_t num_threads = std::thread::hardware_concurrency())
+  parallel_split_and_reduce (std::uint32_t num_threads = std::thread::hardware_concurrency(), std::uint32_t task_load_factor = 64)
   : m_num_threads { num_threads },
-    m_num_waiting { num_threads * 16u },
-    m_thread_pool { num_threads },
-    m_merge_queue { num_threads, 16u }
+    m_num_waiting { num_threads * task_load_factor },
+    m_thread_pool { num_threads }
   {}
 
-  auto apply_to_file_at_path(std::filesystem::path file_name, std::size_t block_size = 1024*1024)  
+  auto apply_to_file_at_path(std::filesystem::path file_name, std::size_t block_size = 64*1024*1024)  
     -> reduce_target_type
   {
     using namespace std;
@@ -48,74 +47,62 @@ struct parallel_split_and_reduce: pinned_object
     chunk_loader the_chunk_loader { file_name, the_chunk_size };    
     reduce_target_type lhs, rhs;    
     deque<future<reduce_target_type>> partial_sets;
-    deque<reduce_target_type> ready_partial_sets;
+    vector<reduce_target_type> ready_partial_sets;
 
     while (!the_chunk_loader.empty())
     {
       m_num_waiting.acquire();
+
+      while (!partial_sets.empty() && is_future_ready (partial_sets.front()) && ready_partial_sets.size() < 2)
+      {
+        ready_partial_sets.emplace_back (partial_sets.front().get());
+        partial_sets.pop_front();        
+      }
+      
+      if (ready_partial_sets.size() > 1)
+      {
+        auto the_future = m_thread_pool.async ([this] (auto the_sets) 
+          -> reduce_target_type
+        {
+          m_num_waiting.release();
+          return collapse_mulltiple_sets(the_sets);
+        }, move (ready_partial_sets));
+        continue;
+      }
+      
+
       chunk_loader::shared_chunk_type the_chunk;
       if (!(the_chunk = the_chunk_loader.next_shared(' ')))
         break;
       auto the_future = m_thread_pool.async ([this, the_chunk { std::move (the_chunk) }] () ->
         reduce_target_type
       {        
-        m_num_waiting.release();
+        m_num_waiting.release();        
         return reduce_chunk_to_word_set(*the_chunk);
       });
 
-      partial_sets.emplace_back (move (the_future));  
+      partial_sets.emplace_back (move (the_future));      
+    }
 
-      while (is_future_ready (partial_sets.front()))
+    while(partial_sets.size () > 1 )
+    {            
+      while (ready_partial_sets.size() < 2)
       {
         ready_partial_sets.emplace_back (partial_sets.front().get());
         partial_sets.pop_front();        
       }
 
-      if (ready_partial_sets.size() > 1)
-      {
-        m_num_waiting.acquire();
-
-        auto the_pair = make_shared<reduce_merge_type> (
-          move (ready_partial_sets.front()), 
-          move (ready_partial_sets.back()));
-
-        ready_partial_sets.pop_front();
-        ready_partial_sets.pop_back();
-
-        auto the_future = m_thread_pool.async ([this] (auto the_pair) 
-          -> reduce_target_type
-        {
-          m_num_waiting.release();
-          return collapse_word_sets_into_one(*the_pair);
-        }, move (the_pair));
-        
-      }
-    }
-    // TODO fix ready_partial_sets + partial_sets
-    partition(partial_sets.begin(), partial_sets.end(), is_future_ready);
-    while(partial_sets.size () > 1 )
-    {            
       m_num_waiting.acquire();
-
-      auto lhs = move (partial_sets.back());
-      partial_sets.pop_back();
-      auto rhs = move (partial_sets.back());
-      partial_sets.pop_back();      
-
-      auto the_pair = make_shared<reduce_merge_type> (lhs.get(), rhs.get());
-      
-      auto the_future = m_thread_pool.async ([this] (auto the_pair) 
+      auto the_future = m_thread_pool.async ([this] (auto the_sets) 
         -> reduce_target_type
       {
         m_num_waiting.release();
-        return collapse_word_sets_into_one (*the_pair);
-      }, move (the_pair));
+        return collapse_mulltiple_sets(the_sets);
+      }, move (ready_partial_sets));
 
       partial_sets.emplace_back (move (the_future));
-
-
+      ready_partial_sets.reserve(10);
     } 
-    assert(partial_sets.size() == 1);
     return partial_sets.back().get();
   }
 
@@ -128,20 +115,19 @@ struct parallel_split_and_reduce: pinned_object
     return ws_local;
   }
 
-  auto collapse_word_sets_into_one(reduce_merge_type& the_merge)  
+  auto collapse_mulltiple_sets(std::vector<reduce_target_type>& the_merge)  
     -> reduce_target_type
   {
-    auto [lhs, rhs] = std::move (the_merge);
-    lhs.merge (rhs);
-    return lhs;
+    reduce_target_type the_result;
+    for (auto& item : the_merge)
+      the_result.merge (move (item));
+    return the_result;
   }
 
 private:
   using semaphore_type = std::counting_semaphore<>;
-  using paco_queue = parallel_concurrent_queue<reduce_target_type>;
 
   const std::size_t m_num_threads;
   semaphore_type m_num_waiting;
   parallel_task_dispatch m_thread_pool;
-  paco_queue m_merge_queue;
 };
